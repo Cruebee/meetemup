3f9237dc6940d481ae57039580fa9a1b2af5907d
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-06-04 14:38:26 -0600

    testing feature 1 scenario 1, added EventList and Event Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import EventList from './components/EventList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <EventList />
       </div>
     );
   }
---
+++ src/components/Event.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Event extends Component {
+
+  state = {
+    events: [],
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Event;
