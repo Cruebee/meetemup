f3664c7b121abe5d59806ea9afe505934b12c98d
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-06-04 16:10:56 -0600

    implement logic to pass second test (citysearch)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import EventList from './components/EventList';
+import CitySearch from './components/CitySearch';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <EventList />
+        <CitySearch />
       </div>
     );
   }
---
+++ src/components/CitySearch.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class CitySearch extends Component {
+
+  render() {
+    return (
+      <div className="CitySearch">
+        <input className="CitySearch_input"></input>
+      </div>
+    );
+  }
+}
+
+export default CitySearch;
