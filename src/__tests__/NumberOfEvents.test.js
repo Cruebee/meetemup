c4cbf89aa38a5938d4661b64f2634d7f8d90e9b3
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-06-07 17:23:48 -0600

    implement tests and passing code for number of events component

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -8,8 +8,29 @@ describe('<NumberOfEvents /> component', () => {
     NumberOfEventsWrapper = shallow(<NumberOfEvents />);
   });
 
-  test('render number of events component', () => {
-    expect(NumberOfEventsWrapper).toHaveLength(1);
+  test('render textbox for user input', () => {
+    expect(NumberOfEventsWrapper.find('.numberOfEvents')).toHaveLength(1);
+  });
+
+  test('check number of events is 32 by default', () => {
+    const numberOfEvents = NumberOfEventsWrapper.state('numberOfEvents');
+    expect(NumberOfEventsWrapper.find('.numberOfEvents__input').prop('value')).toBe(numberOfEvents);
+  });
+
+  test('check that state changes when input changes', () => {
+    const eventObject = { target: { value: 32 } };
+    NumberOfEventsWrapper.find('.numberOfEvents__input').simulate('change', eventObject);
+    expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(32);
+  });
+
+  test('check that state changes when input changes', () => {
+    const eventObject = { target: { value: 24 } };
+    NumberOfEventsWrapper.find('.numberOfEvents__input').simulate('change', eventObject);
+    expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(24);
+  });
+
+  test('render number of events input label', () => {
+    expect(NumberOfEventsWrapper.find('.numberOfEvents label')).toHaveLength(1);
   });
 
 });
--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -2,9 +2,26 @@ import React, { Component } from 'react';
 
 class NumberOfEvents extends Component {
 
+  state = {
+    numberOfEvents: 32
+  }
+
+  handleInputChange = (event) => {
+    const value = event.target.value;
+    this.setState({ numberOfEvents: value });
+  }
+
   render() {
     return (
-      <div></div>
+      <div className="numberOfEvents">
+        <label>Number of Events: </label>
+        <input
+          type="text"
+          className="numberOfEvents__input"
+          value={this.state.numberOfEvents}
+          onChange={this.handleInputChange}
+        />
+      </div>
     );
   }
 }
