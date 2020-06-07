7a97c2442295165a60f3c110126b2b10d99169f3
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-06-07 16:16:26 -0600

    fully implement tests and passing code for Event component

--- src/components/Event.js
+++ src/components/Event.js
@@ -2,11 +2,30 @@ import React, { Component } from 'react';
 
 class Event extends Component {
 
+  state = {
+    event: {},
+    showDetails: false
+  }
+
+  handleShowDetails = () => {
+    this.setState({ showDetails: true });
+  }
+
   render() {
+    const showDetails = this.state.showDetails;
 
     return (
       <div className="event">
-        <div></div>
+        <div className="eventOverview">
+          <p className="eventOverview--name">{this.state.event.name}</p>
+          <p className="eventOverview--localDate">{this.state.event.local_date}</p>
+          <button onClick={() => this.handleShowDetails()}>Event Details</button>
+        </div>
+        {showDetails &&
+          <div className="eventDetails">
+            <p className="eventDetails--description">{this.state.event.description}</p>
+          </div>
+        }
       </div>
     );
   }
