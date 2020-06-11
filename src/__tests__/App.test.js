6fe49023a6d44ab7c9471e1560e09cec2019ffb7
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-06-11 16:32:55 -0600

    update CitySearch component and test file, as well as App test to use mock data and test new lat and lon props needed to call the actual API methods, for future use

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -35,8 +35,9 @@ describe('<App /> integration', () => {
     AppWrapper.instance().updateEvents = jest.fn();
     AppWrapper.instance().forceUpdate();
     const CitySearchWrapper = AppWrapper.find(CitySearch);
-    CitySearchWrapper.instance().handleItemClicked('value');
+    CitySearchWrapper.instance().handleItemClicked('value', 1.1, 1.2);
     expect(AppWrapper.instance().updateEvents).toHaveBeenCalledTimes(1);
+    expect(AppWrapper.instance().updateEvents).toHaveBeenCalledWith(1.1, 1.2);
   });
 
 });
