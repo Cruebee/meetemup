9a1c521315106e5ef1fe081475c9a5f15389f6ea
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-06-17 16:41:53 -0600

    wrap up Acceptance testing for First Feature of app

--- src/features/filterEventsByCity.feature
+++ src/features/filterEventsByCity.feature
@@ -11,8 +11,8 @@ Feature: Filter Events By City
     Then the user should receive a list of cities (suggestions) that match what they've typed
 
   Scenario: User can select a city from the list of suggestions
-    Given a user is typing “Denver” in the city textbox
+    Given a user is typing “Munich” in the city textbox
     And the list of suggested cities is showing
-    When the user selects a city (e.g., “Denver, Colorado”) from the list
-    Then their city should be changed to the selected city (i.e., “Denver, Colorado”)
+    When the user selects a city (e.g., “Munich, Germany”) from the list
+    Then their city should be changed to the selected city (i.e., “Munich, Germany”)
     And the user should receive a list of upcoming events for that city
--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -1,4 +1,9 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import App from '../App';
+import CitySearch from '../components/CitySearch';
+import { mockEvents } from '../mock_events/mock-events';
 
 const feature = loadFeature('./src/features/filterEventsByCity.feature');
 
@@ -9,48 +14,56 @@ defineFeature(feature, test => {
 
     });
 
+    let AppWrapper;
     when('the user opens the app', () => {
-
+      AppWrapper = mount(<App />);
     });
 
     then('the user should see the list of upcoming events from their location', () => {
-
+      AppWrapper.update();
+      expect(AppWrapper.find('.Event')).toHaveLength(mockEvents.events.length);
     });
   });
 
+  let CitySearchWrapper;
   test('User should see a list of suggestions when they search for a city', ({ given, when, then }) => {
     given('the main page is open', () => {
-
+      CitySearchWrapper = shallow(<CitySearch />);
     });
 
     when('the user starts typing in the textbox', () => {
-
+      CitySearchWrapper.find('.city').simulate('change', { target: { value: 'Munich' } });
     });
 
     then('the user should receive a list of cities (suggestions) that match what they\'ve typed', () => {
-
+      expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(2);
     });
   });
 
   test('User can select a city from the list of suggestions', ({ given, and, when, then }) => {
-    given('a user is typing “Denver” in the city textbox', () => {
 
+    let AppWrapper;
+    given('a user is typing “Munich” in the city textbox', () => {
+      AppWrapper = mount(<App />);
+      AppWrapper.find('.city').simulate('change', { target: { value: 'Munich' } });
     });
 
     and('the list of suggested cities is showing', () => {
-
+      AppWrapper.update();
+      expect(AppWrapper.find('.suggestions li')).toHaveLength(2);
     });
 
-    when('the user selects a city (e.g., “Denver, Colorado”) from the list', () => {
-
+    when('the user selects a city (e.g., “Munich, Germany”) from the list', () => {
+      AppWrapper.find('.suggestions li').at(0).simulate('click');
     });
 
-    then('their city should be changed to the selected city (i.e., “Denver, Colorado”)', () => {
-
+    then('their city should be changed to the selected city (i.e., “Munich, Germany”)', () => {
+      const CitySearchWrapper = AppWrapper.find(CitySearch);
+      expect(CitySearchWrapper.state('query')).toBe('Munich, Germany');
     });
 
     and('the user should receive a list of upcoming events for that city', () => {
-
+      expect(AppWrapper.find('.Event')).toHaveLength(mockEvents.events.length);
     });
   });
 
