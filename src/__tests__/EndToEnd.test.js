e56b86b57ad00d942069d7f4fef568fabdd27a01
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-06-18 13:18:40 -0600

    finish walkthrough section of 4.5

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -6,11 +6,14 @@ describe('show/hide an event details', () => {
   let browser;
   let page;
   beforeAll(async () => {
-    browser = await puppeteer.launch();
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 250 // slow down by 250ms
+    });
     page = await browser.newPage();
     await page.goto('http://localhost:3000/');
     await page.waitForSelector('.Event');
-  });
+  }, 60000); // The addition of the 60000 here is to change the default Jest timeout when running tests (default is 50000, which was causing tests to fail)
 
   afterAll(() => {
     browser.close();
@@ -26,6 +29,13 @@ describe('show/hide an event details', () => {
 
     const extra = await page.$('.Event .extra');
     expect(extra).toBeDefined();
+  });
+
+  test('User can collapse an event to hide its details', async () => {
+    await page.click('.Event .details-btn');
+
+    const extra = await page.$('.Event .extra');
+    expect(extra).toBeNull();
   });
 
 })
