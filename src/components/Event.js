00c108dcc38171d2b1fafd140647c03c1fe5bb21
Cruebee <60672128+Cruebee@users.noreply.github.com>
2020-06-29 11:39:33 -0600

    attempt to fix crash

--- src/components/Event.js
+++ src/components/Event.js
@@ -56,7 +56,7 @@ class Event extends Component {
             }
             <ResponsiveContainer height={180}>
               <PieChart>
-                <Pie data={this.getPieData()} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={32} label >
+                <Pie data={this.getPieData()} dataKey={"value"} nameKey="name" cx="50%" cy="50%" outerRadius={32} label >
                   {
                     data01.map((entry, index) => (
                       <Cell key={`cell-${index}`} fill={colors[index]} />))
